eb0df51091cb1e33cce25a0de193b6b0e76fcbb8
Chris Blazek <chris.blazek@gmail.com>
2012-07-12 13:26:57 -0500

    Added Segue, graph view, axes files

---
+++ CalculatorGraphing/GraphView/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  Calculator
+//
+//  Created by Blazek Chris on 7/12/12.
+//  Copyright (c) 2012 628 Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ CalculatorGraphing/GraphView/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewControllerViewController.h
+//  Calculator
+//
+//  Created by Blazek Chris on 7/12/12.
+//  Copyright (c) 2012 628 Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
