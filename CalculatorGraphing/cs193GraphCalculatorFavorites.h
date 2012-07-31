148b220aa6053ff9a4153243312d9ab980ff517e
Chris Blazek <chris.blazek@gmail.com>
2012-07-30 21:16:54 -0500

    favorites walk through

---
+++ CalculatorGraphing/cs193GraphCalculatorFavorites.h
@@ -0,0 +1,13 @@
+//
+//  cs193GraphCalculatorFavorites.h
+//  Calculator
+//
+//  Created by Chris Blazek on 7/30/12.
+//  Copyright (c) 2012 628 Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cs193GraphCalculatorFavorites : UITableViewController
+
+@end
