3836b2c08f2e311b2ac50d7ae996a395f5833f53
Chris Blazek <chris.blazek@gmail.com>
2012-08-06 22:25:09 -0500

    flickr monster with start of geo work

---
+++ FlickrMonsterGeo/FlickrMonster/cs193FlickrMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  cs193FlickrMapViewController.h
+//  FlickrMonster
+//
+//  Created by Chris Blazek on 8/6/12.
+//  Copyright (c) 2012 Chris Blazek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cs193FlickrMapViewController : UIViewController
+
+@end
---
+++ FlickrMonsterGeo/FlickrMonster/cs193FlickrMapViewController.m
@@ -0,0 +1,43 @@
+//
+//  cs193FlickrMapViewController.m
+//  FlickrMonster
+//
+//  Created by Chris Blazek on 8/6/12.
+//  Copyright (c) 2012 Chris Blazek. All rights reserved.
+//
+
+#import "cs193FlickrMapViewController.h"
+
+@interface cs193FlickrMapViewController ()
+
+@end
+
+@implementation cs193FlickrMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
